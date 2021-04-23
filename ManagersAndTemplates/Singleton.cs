b7c86384f9f6dbbdd064f8c429d9201314fef59f
EliasFIVE <57570251+EliasFIVE@users.noreply.github.com>
2021-04-23 22:39:37 +0300

    add manager and singltone template

---
+++ ManagersAndTemplates/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+///<summary>
+///Singleton class template
+///Controls instantiation of a single instance of class
+///</summaty>
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get { return instance; }
+    }
+
+    public static bool IsInitialized
+    {
+        get { return instance != null; }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("[Singleton] Trying to instantiate a second instance of a singleton class.");
+        }
+        else
+        {
+            instance = (T) this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
